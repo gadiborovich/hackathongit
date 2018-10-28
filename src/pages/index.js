1a5f50bb2f81a71322f107dfe611b1b07447af19
Your Name <gadi.borovich@gmail.com>
2018-10-27 23:36:05 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>My name is Gadi Borovich, I would write Hello World but 
+      I dont think its original.</p>
+    <p>Now I will build something great.</p>
+  </Layout>
+)
+
+export default IndexPage
